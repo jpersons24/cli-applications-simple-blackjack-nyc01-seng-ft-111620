e4004e48d2fd968e38c835a2f549b7da7d08dae7
Jakob Persons <jakobep24@gmail.com>
2020-11-09 11:26:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,38 +1,64 @@
+require 'pry'
+
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
 end
 
-def hit?
+def hit?(current_card_total)
   # code hit? here
+  prompt_user
+  user_input = get_user_input
+  current_card_total
+
+  if user_input = 'h'
+    deal_card
+  else user_input != 's' || 'h'
+    invalid_command
+    prompt_user
+    get_user_input
+  end
+  current_card_total
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
+
+# binding.pry
 
 #####################################################
 # get every test to pass before coding runner below #
@@ -41,4 +67,3 @@ end
 def runner
   # code runner here
 end
-    
